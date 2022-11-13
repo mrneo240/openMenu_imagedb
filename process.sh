68889c558d72ff701983b4e0969b01f706bc706d
Hayden Kowalchuk <819028+mrneo240@users.noreply.github.com>
2022-11-12 23:48:00 -0500

    feat: the whole thing...

--- process.sh
+++ process.sh
@@ -3,7 +3,13 @@ OUT=output
 TOOLS=tools
 
 # process images to proper format and location
-./process_EX.sh
+./process_EX.sh # Unreleased, demos, debug, etc...
+./process_HB.sh # Homebrew Covers
+./process_AW_NTSC.sh # Atomiswave with NTSC style covers
+#./process_AW_PAL.sh # Atomiswave with PAL style covers
+./process_USA.sh # Commercial USA
+./process_PAL.sh # Commercial PAL
+./process_JAP.sh # Commercial JAP
 
 # pack DAT
 ./pack_DAT.sh
---
+++ process_AW_PAL.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+TOOLS=tools
+CAT=catalogues/product_aw.csv
+IN=AW_PAL_input
+OUT_BOX=BOX_all
+OUT_ICON=ICON_all
+
+# Boxart
+mkdir -p ${OUT_BOX}
+
+mogrify -path ${OUT_BOX} -resize 128x128! -format bmp -define bmp:format=bmp3 -type truecolor -depth 8 ${IN}/*
+${TOOLS}/stbconv -t -5 -nm -gi 1001 ${OUT_BOX}/*.bmp
+rm ${OUT_BOX}/*.bmp
+${TOOLS}/renamecsv ${OUT_BOX}/ ${CAT}
+# pvr format: no mipmap, twiddled, rgb565, global index 1001
+
+#Icon
+mkdir -p ${OUT_ICON}
+
+mogrify -path ${OUT_ICON} -resize 256x256! -format bmp -define bmp:format=bmp3 -type truecolor -depth 8 ${IN}/*
+${TOOLS}/stbconv -t -5 -nm -gi 1001 ${OUT_ICON}/*.bmp
+rm ${OUT_ICON}/*.bmp
+${TOOLS}/renamecsv ${OUT_ICON}/ ${CAT}
+# pvr format: no mipmap, twiddled, rgb565, global index 1001
