74f2f65400ec903915df55e692ec558239f814ea
Hayden Kowalchuk <819028+mrneo240@users.noreply.github.com>
2022-11-27 16:20:07 -0500

    fix: correct naming of folders

--- process_EX.sh
+++ process_EX.sh
@@ -8,7 +8,7 @@ OUT_ICON=ICON_all
 # Boxart
 mkdir -p ${OUT_BOX}
 
-mogrify -path ${OUT_BOX} -resize 128x128! -format bmp -define bmp:format=bmp3 -type truecolor -depth 8 ${IN}/*
+mogrify -path ${OUT_BOX} -resize 256x256! -format bmp -define bmp:format=bmp3 -type truecolor -depth 8 ${IN}/*
 ${TOOLS}/stbconv -t -5 -nm -gi 1001 ${OUT_BOX}/*.bmp
 rm ${OUT_BOX}/*.bmp
 ${TOOLS}/renamecsv ${OUT_BOX}/ ${CAT}/product_ex.csv
@@ -17,7 +17,7 @@ ${TOOLS}/renamecsv ${OUT_BOX}/ ${CAT}/product_ex.csv
 #Icon
 mkdir -p ${OUT_ICON}
 
-mogrify -path ${OUT_ICON} -resize 256x256! -format bmp -define bmp:format=bmp3 -type truecolor -depth 8 ${IN}/*
+mogrify -path ${OUT_ICON} -resize 128x128! -format bmp -define bmp:format=bmp3 -type truecolor -depth 8 ${IN}/*
 ${TOOLS}/stbconv -t -5 -nm -gi 1001 ${OUT_ICON}/*.bmp
 rm ${OUT_ICON}/*.bmp
 ${TOOLS}/renamecsv ${OUT_ICON}/ ${CAT}/product_ex.csv
